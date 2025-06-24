6935f86b3b3a609b1d6552bffadccb021ef9fd47
Jacob Williams <jayteedubya@gmail.com>
2025-06-23 22:29:29 -0700

    added basic http responses to all apps

--- portfolio/blog/views.py
+++ portfolio/blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 
 # Create your views here.
+def blog_home(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("Welcome to the Blog Home Page")
--- portfolio/guide/views.py
+++ portfolio/guide/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 
 # Create your views here.
+def guide_home(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("Welcome to the guide Home Page")
